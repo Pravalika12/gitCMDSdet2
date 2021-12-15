aaa6d58406212354285137194faa1de69f48da36
Pravalika12 <pravalikareddyerra@gmail.com>
2021-12-15 12:48:09 +0530

    new

---
+++ src/test/java/gitPractice/gitBash.java
@@ -0,0 +1,7 @@
+package gitPractice;
+
+public class gitBash {
+public static void main(String[] args) {
+	System.out.println("git commands practice started");
+}
+}
---
+++ src/test/java/gitPractice/gitbash4.java
@@ -0,0 +1,7 @@
+package gitPractice;
+
+public class gitbash4 {
+	public static void main(String[] args) {
+		System.out.println("4th code success");
+	}
+}
